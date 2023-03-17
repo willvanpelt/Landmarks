f8e4a77615dcbf72869a4b91cf8b7540ef0a3bff
Will Van Pelt <willvanpelt@protonmail.com>
2023-03-17 17:00:28 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Will Van Pelt on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
